1adb4416852bfa0d264b732dccb8da41a9e2ff1b
pnkrc <pnkrc.c@gmail.com>
2025-04-03 00:38:29 -0300

    Fix index issue

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import ChampieSearch from './components/ChampieSearch.vue'
     class="min-h-screen cursor-default bg-amber-200 pb-8 font-serif dark:bg-sky-900 dark:text-white"
   >
     <header
-      class="sticky top-0 z-10 flex items-center justify-between bg-amber-300 px-8 py-4 dark:bg-sky-950"
+      class="sticky top-0 z-100 flex items-center justify-between bg-amber-300 px-8 py-4 dark:bg-sky-950"
     >
       <h1 class="text-2xl font-black uppercase">Champie tool</h1>
       <ChampieSearch />
